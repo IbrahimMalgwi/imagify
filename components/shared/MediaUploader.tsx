3a27079d8a069aa69e612e080edd6968b3298c2f
IbrahimMalgwi <ibrahimganamalgwi@outlook.com>
2024-02-21 16:28:15 +0100

    medai uploader

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_imaginify"
+      uploadPreset="igm_imagify"
       options={{
         multiple: false,
         resourceType: "image",
